c1239bfc19fd05d50aa88b986e01b653338b2d2b
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-03-13 13:02:45 -0300

    Finaliza programa Medidas

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+          string medida;
+          double metros, cm, km;
+
+          Console.WriteLine();
+          Console.WriteLine("--- Exercício Medidas ---");
+          Console.WriteLine("Descreva um valor para que possa ser exibido de Metros a Centímetros e Kilômetros...");
+          medida = Console.ReadLine(); 
+          Console.WriteLine();
+          
+          metros = Convert.ToDouble(medida);
+          cm = metros*1000;
+          km = metros/1000;
+          Console.WriteLine();
+
+
+          Console.WriteLine($"A medida em Centímetros é: {cm:N}");
+          Console.WriteLine($"A medida em Kilômetros é: {km:N4}");
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
